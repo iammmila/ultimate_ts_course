2d1211d1dc284fd9d0eb2eb9bc5d0ae0c41124c4
iammmila <ibramila2002@gmail.com>
2023-08-02 21:30:44 +0400

    4.8. Nullish Coaelscing Operator

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
-type Customer = {
-  birthday?: Date;
+let speed: number | null = null;
+let ride = {
+  // Falsy(undefined, null, '', false, 0)
+  // Nullish coalescing operator
+  speed: speed ?? 30,
+  // ! this means that: if speed is not null or undefined, use that value. otherwise, use 30 as a default value
+  
 };
-function getCustomer(id: number): Customer | null | undefined {
-  return id === 0 ? null : { birthday: new Date() };
-}
-let customer = getCustomer(1);
-// if (customer !== null && customer!==undefined) {
-// optional property access operator
-console.log(customer?.birthday?.getFullYear());
-// }
-
-// optional element access operator
-// customers?.[0]
-
-// optional call
-let log: any = null;
-log?.("a");
