5b42a3b61e9f755f6f55c58f0f0027d5b347cf68
iammmila <ibramila2002@gmail.com>
2024-02-17 16:39:30 +0100

    5.6. Access Control Keywords

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,17 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error("invalid balance");
-        this.balance += amount;
+        this._balance += amount;
+    }
+    getBalance() {
+        return this._balance;
     }
 }
 let account1 = new Account(1, "Mila", 0);
-account1.deposit(100);
-console.log(account1);
-console.log(account1.balance);
-console.log(typeof account1);
-console.log(account1 instanceof Account);
+console.log(account1.nickname);
+console.log(account1.getBalance());
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,24 @@
 class Account {
   readonly id: number; // * readonly - we cannot change this id
   owner: string;
-  balance: number;
+  private _balance: number;
   nickname?: string; // * optional properties
 
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = balance;
   }
 
   deposit(amount: number): void {
     if (amount <= 0) throw new Error("invalid balance");
-    this.balance += amount;
+    this._balance += amount;
+  }
+  getBalance(): number {
+    return this._balance;
   }
 }
 
 let account1 = new Account(1, "Mila", 0);
-account1.deposit(100);
-console.log(account1);
-console.log(account1.balance);
-console.log(typeof account1); //object
-console.log(account1 instanceof Account); //true
-// !In JavaScript, the instanceof operator is used to check
-//!  whether an object is an instance of a particular class or constructor function.
-// !  It returns true if the object is an instance of the specified class, otherwise it returns false.
+console.log(account1.nickname); //undefined
+console.log(account1.getBalance()); // 0
