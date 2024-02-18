0ebc46a02e9dc4e794a1879a3fd0c89ad2ee8d6c
iammmila <ibramila2002@gmail.com>
2024-02-18 16:34:53 +0100

    7.3. Class Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,17 @@
-//exercise 1
-function echo<T>(arg: T): T {
-  return arg;
-}
-
-//exercise 2
-// When compiling the following piece of code, we get an error saying ‘Property name
-// does not exist on type T’. How can we solve this problem?
-// function printName<T>(obj: T) {
-//   console.log(obj.name);
+//!EXAMPLE 1
+// class Component {
+//   insertInDOM() {}
 // }
-function printName<T extends { name: string }>(obj: T) {
-  console.log(obj.name);
+// class ProfileComponent extends Component{}
+
+//!EXAMPLE 2
+function Component(constructor: Function) {
+  console.log("Component decorato₹ called");
+  constructor.prototype.uniqueId = Date.now();
+  constructor.prototype.insertInDOM = () => {
+    console.log("inserting the component in the DOM");
+  };
 }
 
-//exercise 3
-// An Entity should have a unique identifier. The type of identifier, however, is dependent
-// on the use case. In some cases, the ID might be a number, in other cases, it might be a
-// string, GUID, etc. Represent the entity using a generic class.
-class Entity<T> {
-  constructor(id: T) {}
-}
-
-//exercise 4
-// Given the following interface, what does keyof User return?
-// interface User {
-// userId: number;
-// username: string;
-// }
-interface User {
-  userId: number;
-  username: string;
-} // keyof user: 'userId' | 'username'
+@Component
+class ProfileComponent {}
