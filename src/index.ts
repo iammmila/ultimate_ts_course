d74f4c1b63cde134fa19b02a2e0deda09ecb18d9
iammmila <ibramila2002@gmail.com>
2024-02-18 17:12:47 +0100

    7.7. Accessor Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
-function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
-  const original = descriptor.value as Function;
-  descriptor.value = function (...args: any) {
-    console.log("before");
-    original.call(this, ...args);
-    console.log("after");
+function Capitalize(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  // const original = descriptor.value //it is not working for getters and setters; it is working for only regular methods;
+  const original = descriptor.get;
+  descriptor.get = function () {
+    const result = original?.call(this); //optional chaining - because of original can be undefined
+    // original!.call(this); //exclamation mark - because we say: hey i know it is not gonna be undefined or null;
+    return typeof result === "string" ? result.toUpperCase() : result;
   };
 }
+
 class Person {
-  @Log
-  say(message: string) {
-    console.log("person say: " + message);
+  constructor(public firstName: string, public lastName: string) {}
+  @Capitalize
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
   }
 }
 
-let person = new Person();
-person.say("Hello"); // it will be ignored, because of the blue sky;
-
-//!output:
-// before
-// person say: blue sky
-// after
+let person = new Person("mila", "ibrahimova");
+console.log(person.fullName);
