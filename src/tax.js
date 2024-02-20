bb28e8ee0cbde883d3027256d0a20f4b0356e7dd
iammmila <ibramila2002@gmail.com>
2024-02-20 13:57:52 +0100

    9.5. Creating Declaration Files

--- src/tax.js
+++ src/tax.js
@@ -1,9 +1,3 @@
-/**
- *Calculates the incomes tax
- * @param {number} income - Net salary after expenses
- * @returns {number}
- */
-
 export function calculateTax(income) {
   return income * 0.3;
 }
