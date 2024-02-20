86200cf8213a6d2333e01215863808ce7b55301a
iammmila <ibramila2002@gmail.com>
2024-02-20 21:43:38 +0100

    10.5. Using The State Hook

--- reminders-app/src/App.tsx
+++ reminders-app/src/App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
 
-const reminders: Reminder[] = [{ id: 1, title: "list" }];
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id: 1, title: "title 1" },
+  ]);
+
   return (
     <div className="App">
       <ReminderList items={reminders} />
