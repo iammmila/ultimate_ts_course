ee7c668bf853ccb1046c5d0257ba220835613332
iammmila <ibramila2002@gmail.com>
2024-02-25 17:10:36 +0100

    11.2. Executing TypeScript Code With Node

---
+++ reminders-api/index.ts
@@ -0,0 +1,8 @@
+let x: number = 10;
+console.log(x);
+
+//firstly we write in cmd: npm init -y
+// then: npm i -D ts-node
+//then we configure in the package.json : start: ts-node index.ts
+// now in terminal we can write : npm start;
+// after this we can see our result in termainl;
