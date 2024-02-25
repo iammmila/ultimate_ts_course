1b3afe949164ca5ad122cc271d149b571f58dfa8
iammmila <ibramila2002@gmail.com>
2024-02-25 17:41:10 +0100

    11.6. Parsing Request Bodies

---
+++ reminders-api/dtos/create-reminder.ts
@@ -0,0 +1,3 @@
+export default interface CreateReminderDTO {
+  title: string;
+}
--- reminders-api/routers/reminders.ts
+++ reminders-api/routers/reminders.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import CreateReminderDTO from "../dtos/create-reminder";
+
 const router = Router();
 
 router.get("/", (req, res) => {
   res.send("List Of Reminders");
+});
+
+router.post("/", (req, res) => {
+  const { title } = req.body as CreateReminderDTO;
+  res.json(title);
 });
 
 export default router;
